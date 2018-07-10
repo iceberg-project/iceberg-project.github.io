777a6c374e11c9e07a0a6dac39f0ad0defa69448
Bradley D Spitzbart <bspitzbart@stonybrook.edu>
2018-07-10 04:33:39 -0400

    add EarthCube poster and publications page

--- build_site.R
+++ build_site.R
@@ -1,3 +1,4 @@
 #setwd("~/iceberg-project.github.io")
-setwd("~/Documents/Projects/iceberg-project.github.io")
+#setwd("~/Documents/Projects/iceberg-project.github.io")
+setwd("./")
 rmarkdown::render_site()
