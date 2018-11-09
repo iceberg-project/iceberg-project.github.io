7ba64c60f144b8d261758cd65ce27f68b99ca2e3
yagrawala <eternalyogi@Yogeshs-MacBook-Pro.local>
2018-11-09 12:19:19 -0500

    changes for new pages

--- Test/js/publications.js
+++ Test/js/publications.js
@@ -1,213 +1,17 @@
 var publication_info = [
 	{
-		"title"		: "Chemical-free n-type and p-type multilayer-graphene transistors",
-		"url"		: "http://aip.scitation.org/doi/10.1063/1.4960530",
-		"authors"	: ["D. M. N. M. Dissanayake", "and M. D. Eisaman"],
-		"source"	: "Appl. Phys. Lett. 109, 053110 (2016)",
-		"image"		: "pub27.gif",
-		"tags"		: ["Graphene"],
-		"press"		: [],
-		"timestamp"	: "07/01/2016"
+		"title"		: "Black-swan events: Population crashes or temporary emigration?",
+		"url"		: "http://www.pnas.org/content/early/2017/10/11/1713621114.full",
+		"authors"	: ["Youngflesh, C.", "and H.J. Lynch"],
+		"source"	: "PNAS October 24, 2017 114 (43) E8953-E8954",
+		"timestamp"	: "10/12/2017"
 	},
 	{
-		"title"		: "Spontaneous and strong multi-layer graphene n-doping on soda-lime glass and its application in graphene-semiconductor junctions",
-		"url"		: "https://www.nature.com/articles/srep21070",
-		"authors"	: ["D. M. N. M. Dissanayake,", "A. Ashraf,", "D. Dwyer,", "K. Kisslinger,", "L. Zhang,", "Y. Pang,", "H. Efstathiadis", "and M. D. Eisaman"],
-		"source"	: "Scientific Reports, 6, 21070 (2016)",
-		"image"		: "graphene-figure-hr.jpg",
-		"tags"		: ["Graphene"],
-		"press"		: [	{
-							"title"	: "BNL Press Release",
-							"link"	: "https://www.bnl.gov/newsroom/news.php?a=111814",
-						},
-						{
-							"title"	: "SBU Press Release",
-							"link"	: "http://sb.cc.stonybrook.edu/news/general/2016-02-12-using-glass-to-improve-graphenes-powerful-conductivity.php",
-						}
-					  ],
-		"timestamp"	: "01/18/2016"
-	},
-	{
-		"title"		: "Enhanced photovoltaic performance of ultrathin Si solar cells via semiconductor nanocrystal sensitization: energy transfer vs. optical coupling effects",
-		"url"		: "http://pubs.rsc.org/en/content/articlelanding/2014/NR/C5NR07932B#!divAbstract",
-		"authors"	: ["S. Hoang,", "A. Ashraf,", "M. D. Eisaman,", "D. Nykypanchuk,", "and C. Nam"],
-		"source"	: "Nanoscale, 8, 5873-5883 (2016)",
-		"image"		: "pub25.jpg",
-		"tags"		: ["Photovoltaic"],
-		"press"		: [],
-		"timestamp"	: "03/21/2016"
-	},
-	{
-		"title"		: "Characterization of Cu(In,Ga)Se<sub>2</sub> (CIGS) films with varying gallium ratios",
-		"url"		: "https://www.sciencedirect.com/science/article/pii/S0925838815309828",
-		"authors"	: ["J. Claypoole,", "B. Peace,", "N. Sun,", "D. Dwyer,", "M. D. Eisaman,", "P. Haldar,", "and H. Efstathiadis"],
-		"source"	: "Journal of Alloys and Compounds, DOI: 10.1016 /j.jallcom. 2015.09.006 (2015)",
-		"image"		: "pub24v2.jpg",
-		"tags"		: ["Photovoltaic"],
-		"press"		: [],
-		"timestamp"	: "02/05/2016"
-	},
-	{
-		"title"		: "The effect of confinement on the crystalline microstructure of polymer : fullerene bulk heterojunctions",
-		"url"		: "http://pubs.rsc.org/en/Content/ArticleLanding/2015/CP/C5CP03399C#!divAbstract",
-		"authors"	: ["A. Ashraf,", "D. M. N. M. Dissanayake", "and M. D. Eisaman"],
-		"source"	: "Phys. Chem. Chem. Phys., Advance Article, DOI: 10.1039/C5CP03399C (2015)",
-		"image"		: "pub23.jpg",
-		"tags"		: ["Photovoltaic"],
-		"press"		: [],
-		"timestamp"	: "07/01/2015"
-	},
-	{
-		"title"		: "Measuring charge carrier mobility in photovoltaic devices with micron-scale resolution",
-		"url"		: "http://aip.scitation.org/doi/full/10.1063/1.4915614",
-		"authors"	: ["A. Ashraf,", "D. M. N. M. Dissanayake", "and M. D. Eisaman"],
-		"source"	: "Applied Physics Letters, 106, 113504 (2015)",
-		"image"		: "pub22.jpg",
-		"tags"		: ["Photovoltaic"],
-		"press"		: [],
-		"timestamp"	: "03/01/2015"
-	},
-	{
-		"title"		: "Sub-50-nm self-assembled nanotextures for enhanced broadband antireflection in silicon solar cells",
-		"url"		: "https://www.nature.com/articles/ncomms6963",
-		"authors"	: ["Atikur Rahman,", "Ahsan Ashraf,", "Huolin Xin,", "Xiao Tong,", "Peter Sutter,", "Matthew D. Eisaman", "and Charles T. Black"],
-		"source"	: "Nature Communications, 6, 5963 (2015)",
-		"image"		: "Pub21.jpg",
-		"tags"		: ["Photovoltaic"],
-		"press"		: [	{
-							"title"	: "BNL Press Release",
-							"link"	: "https://www.bnl.gov/newsroom/news.php?a=111685",
-						},
-						{
-							"title"	: "SBU Press Release",
-							"link"	: "http://sb.cc.stonybrook.edu/news/research/150123NanostructureSurfaceTextures.php",
-						},
-						{
-							"title"	: "Newsday Article",
-							"link"	: "https://www.newsday.com/business/brookhaven-national-lab-researchers-improve-solar-panels-with-antireflective-coating-study-finds-1.9827774",
-						},
-						{
-							"title"	: "Phys.org Article",
-							"link"	: "https://phys.org/news/2015-01-self-assembled-nanotextures-antireflective-surface-silicon.html",
-						},
-						{
-							"title"	: "Stony Brook Independent Article",
-							"link"	: "http://www.sbindependent.org/accidental-find-leads-to-great-advancement-for-solar-panels/",
-						}
-					  ],
-		"timestamp"	: "01/21/2015"
-	},
-	{
-		"title"		: "Characterization of plasmonic hole arrays as transparent electrical contacts for organic photovoltaics using high-brightness Fourier transform methods",
-		"url"		: "http://www.tandfonline.com/doi/full/10.1080/09500340.2014.917731",
-		"authors"	: ["F. E. Camino,", "C.Y. Nam,", "Y. T. Pang,", "J. Hoy,", "M. D. Eisaman,", "C. T. Black,", "and M. Y. Sfeir"],
-		"source"	: "J. Mod. Opt., DOI:10.1080/09500340.2014.917731 (2014)",
-		"image"		: "Pub20.jpeg",
-		"tags"		: ["Photovoltaic"],
-		"press"		: [],
-		"timestamp"	: "05/15/2014"
-	},
-	{
-		"title"		: "Index-matched IWKB method for the measurement of spatially varying refractive index profiles within thin-film photovoltaics",
-		"url"		: "https://www.osapublishing.org/oe/abstract.cfm?uri=oe-22-S1-A188",
-		"authors"	: ["Y. T. Pang,", "M. Bossart,", "and M. D. Eisaman"],
-		"source"	: "Optics Express, 22, S1, A188-A197 (2014)",
-		"image"		: "Pub19.jpg",
-		"tags"		: ["Photovoltaic"],
-		"press"		: [],
-		"timestamp"	: "01/13/2014"
-	},
-	{
-		"title"		: "Confinement-Induced Reduction in Phase Segregation and Interchain Disorder in Bulk Heterojunction Films",
-		"url"		: "http://pubs.acs.org/doi/abs/10.1021/nn404172m",
-		"authors"	: ["Ahsan Ashraf,", "D. M. Nanditha", "M. Dissanayake,", "David S. Germack,", "Conan Weiland", "and Matthew D. Eisaman"],
-		"source"	: "ACS Nano, 8, 323-331 (2014)",
-		"image"		: "Pub18.jpg",
-		"tags"		: ["Photovoltaic"],
-		"press"		: [],
-		"timestamp"	: "01/28/2014"
-	},
-	{
-		"title"		: "Mapping Spatially Resolved Charge Collection Probability within P3HT:PCBM Bulk Heterojunction Photovoltaics",
-		"url"		: "http://onlinelibrary.wiley.com/doi/10.1002/aenm.201300525/abstract",
-		"authors"	: ["D. M. N. M. Dissanayake,", "A. Ashraf,", "Y. Pang", "and M. D. Eisaman"],
-		"source"	: "Advanced Energy Materials, 4, 1300525 (2014)",
-		"image"		: "Pub17.jpg",
-		"tags"		: ["Photovoltaic"],
-		"press"		: [	{
-							"title"	: "Inside Front Cover",
-							"link"	: "http://onlinelibrary.wiley.com/store/10.1002/aenm.201470008/asset/aenm201470008.pdf?v=1&t=jd2ljt4j&s=2cd5abd0fe2b8ad44f1b3485c07bc5963483f4fa&systemMessage=Please+be+advised+that+we+experienced+an+unexpected+issue+that+occurred+on+Saturday+and+Sunday+January+20th+and+21st+that+caused+the+site+to+be+down+for+an+extended+period+of+time+and+affected+the+ability+of+users+to+access+content+on+Wiley+Online+Library.+This+issue+has+now+been+fully+resolved.++We+apologize+for+any+inconvenience+this+may+have+caused+and+are+working+to+ensure+that+we+can+alert+you+immediately+of+any+unplanned+periods+of+downtime+or+disruption+in+the+future.",
-						},
-						{
-							"title"	: "BNL Press Release",
-							"link"	: "https://www.bnl.gov/newsroom/news.php?a=111567",
-						},
-						{
-							"title"	: "Solar Novus Article",
-							"link"	: "http://www.solarnovus.com/optoelectronic-technique-illuminates-electron-congestion-in-organic-solar-cells_N6987.html",
-						}
-					  ],
-		"timestamp"	: "08/25/2013"
-	},
-	{
-		"title"		: "Guided-mode quantum efficiency: A novel optoelectronic characterization technique",
-		"url"		: "https://aip.scitation.org/doi/abs/10.1063/1.4768539",
-		"authors"	: ["D. M. N. M. Dissanayake,", "A. Ashraf,", "Y. Pang", "and M. D. Eisaman"],
-		"source"	: "Rev. Sci. Instrum. 83, 114704 (2012)",
-		"image"		: "pub16.jpg",
-		"tags"		: ["Photovoltaic"],
-		"press"		: [],
-		"timestamp"	: "05/15/2012"
-	},
-	{
-		"title"		: "Invited Review Article: Single-photon sources and detectors",
-		"url"		: "https://aip.scitation.org/doi/full/10.1063/1.3610677",
-		"authors"	: ["M. D. Eisaman,", "J. Fan,", "A. Migdall", "and S. V. Polyakov"],
-		"source"	: "Review of Scientific Instruments, 82, 071101 (2011)",
-		"image"		: "Pub12.jpg",
-		"tags"		: ["Photovoltaic"],
-		"press"		: [],
-		"timestamp"	: "05/15/2011"
-	},
-	{
-		"title"		: "Indirect ocean capture of atmospheric CO2: Part I. Prototype of a negative emissions technology",
-		"url"		: "https://www.sciencedirect.com/science/article/pii/S1750583617304322",
-		"authors"	: ["Charles-Francois de Lannoy,", "Matthew D.Eisaman,", "Arun Jose,", "Stephen D.Karnitz,", "Richard W.DeVaul,", "Kathy Hannun", "and Jessy L.B.Rivest"],
-		"source"	: "International Journal of Greenhouse Gas Control, Volume 70 (2017)",
-		"image"		: "pub28.png",
-		"tags"		: ["Negative-Emissions"],
-		"press"		: [],
-		"timestamp"	: "10/16/2017"
-	},
-	{
-		"title"		: "Indirect ocean capture of atmospheric CO2: Part II. Understanding the cost of negative emissions",
-		"url"		: "https://www.sciencedirect.com/science/article/pii/S175058361730436X",
-		"authors"	: ["Matthew D.Eisaman,", "Jessy L.B.Rivest,", "Stephen D.Karnitz,", "Charles-Francois de Lannoy,", "Arun Jose,", "Richard W.DeVaul", "and Kathy Hannun"],
-		"source"	: "International Journal of Greenhouse Gas Control, Volume 70 (2017)",
-		"image"		: "pub29.png",
-		"tags"		: ["Negative-Emissions"],
-		"press"		: [],
-		"timestamp"	: "02/25/2018"
-	},
-	{
-		"title"		: "New Design and Characterization Techniques for Thin-Film Solar Cells",
-		"url"		: "http://graduate.physics.sunysb.edu/announ/theses/pang-yutong-august-2016.pdf",
-		"authors"	: ["Yutong Pang"],
-		"source"	: "PhD Thesis (August 2016)",
-		"image"		: "yutong_thesis.png",
-		"tags"		: ["Photovoltaic"],
-		"press"		: [],
-		"timestamp"	: "08/01/2016"
-	},
-	{
-		"title"		: "Nanoscale Spatial Inhomogeneity in Photovoltaic Devices",
-		"url"		: "http://graduate.physics.sunysb.edu/announ/theses/ashraf-ahsan-may-2016.pdf",
-		"authors"	: ["Ahsan Ashraf"],
-		"source"	: "PhD Thesis (May 2016)",
-		"image"		: "ahsan_thesis.png",
-		"tags"		: ["Photovoltaic"],
-		"press"		: [],
-		"timestamp"	: "05/01/2016"
+		"title"		: "Spatial aggregation reveals robust dynamics despite stochastic noise in pan-Antarctic analysis of Adélie penguin abundance",
+		"url"		: "https://www.nature.com/articles/s41467-017-00890-0",
+		"authors"	: ["Che-Castaldo, C.", "S. Jenouvrier", "C. Youngflesh", "K. Shoemaker", "G. Humphries", "P. McDowall", "L. Landrum", "M. Holland", "Y. Li", "R. Ji", "and H.J. Lynch"],
+		"source"	: "Nature Communicationsvolume 8, Article number: 832 (2017)",
+		"timestamp"	: "10/10/2017"
 	}
 ];
 
@@ -216,7 +20,7 @@ var generate_html = function(publication_sequence) {
 	for(var i=0; i<publication_sequence.length; i++) {
 		html_string += "<div class=\"card bg-publication card-info mb-4\">";
 		html_string += 	"<div class=\"row\">";
-		html_string += 		"<div class=\"col-12 col-md-8\">";
+		html_string += 		"<div class=\"col-12 col-md-12\" style=\"padding: 0px;\">";
 		html_string += 			"<div class=\"card-header\">";
 		html_string += 				"<h5>";
 		html_string += 					"<a class=\"text-primary\" href=\""+ publication_sequence[i]["url"] +"\" target=\"_blank\">";
@@ -224,9 +28,6 @@ var generate_html = function(publication_sequence) {
 		html_string += 					"</a>";
 		html_string += 				"</h5>";
 
-		for(var j=0; j<publication_sequence[i]["tags"].length; j++)
-			html_string += 				"<button type=\"button\" class=\"btn-sm btn-secondary marginRight-16\" disabled>" + publication_sequence[i]["tags"][j] + "</button>";
-		html_string += 				"<button type=\"button\" class=\"btn-sm btn-secondary marginRight-16\" disabled>"+ moment(publication_sequence[i]["timestamp"], "MM-DD-YYYY").year() +"</button>";
 		html_string += 			"</div>";
 		html_string +=			"<ul class=\"list-group list-group-flush\">";
 		html_string +=				"<li class=\"list-group-item\">";
@@ -239,59 +40,22 @@ var generate_html = function(publication_sequence) {
 		html_string +=				"</li>";
 		html_string +=			"</ul>";
 
-		if(publication_sequence[i]["press"].length) {
-			html_string +=			"<div class=\"card-footer\">";
-			for(var j=0; j<publication_sequence[i]["press"].length; j++) {
-				html_string +=			"<a class=\"text-success\" href=\""+ publication_sequence[i]["press"][j]["link"] +"\" target=\"_blank\">";
-				html_string +=				publication_sequence[i]["press"][j]["title"];
-				html_string +=				"<i class=\"fa fa-external-link\" aria-hidden=\"true\"></i>";
-				html_string +=			"</a>";
-				html_string +=			"<br/>";
-			}
-			html_string +=		"</div>";
-		}
-
 		html_string +=		"</div>";
-		html_string +=		"<div class=\"col-md-4\">";
-		if( publication_sequence[i]["image"] )
-			html_string +=			"<img class=\"card-img-top\" src=\"img/"+ publication_sequence[i]["image"] +"\"";
-		html_string +=		"</div>";
-		html_string +=	"</div>";
+		html_string += "</div>";
 		html_string += "</div>";
 		html_string += "</div>";
 	}
 	return html_string;
 };
 
-var sort_publications_by = function(publication_sequence, sortKey) {
-	var keys = sortKey.split(" ");
-	if( keys[0] == "Year" ) {
-		if(keys[1] == "Ascending")
-			publication_sequence.sort( function(a,b) { return moment(a["timestamp"], "MM-DD-YYYY").diff( moment(b["timestamp"], "MM-DD-YYYY") ) } );
-		else if(keys[1] == "Descending")
-			publication_sequence.sort( function(a,b) { return moment(b["timestamp"], "MM-DD-YYYY").diff( moment(a["timestamp"], "MM-DD-YYYY") ) } );
-	}
-	else{
-		// first sort in descending order
-		publication_sequence.sort( function(a,b) { return moment(b["timestamp"], "MM-DD-YYYY").diff( moment(a["timestamp"], "MM-DD-YYYY") ) } );
-		// then check for presence of key in tag
-		var present = [], not_present = [];
-		for(var i=0; i<publication_sequence.length; i++) {
-			if(publication_sequence[i]["tags"].indexOf(keys[0]) != -1)
-				present.push( publication_sequence[i] );
-			else
-				not_present.push( publication_sequence[i] );
-		}
-		publication_sequence = present.concat(not_present);
-	}
+var sort_publications_by = function(publication_sequence) {
 	return publication_sequence;
 };
 
-var setPublications = function(publication_sequence, sort_Key) {
-	var sorted_publications = sort_publications_by(publication_sequence, sort_Key);
+var setPublications = function(publication_sequence) {
+	var sorted_publications = sort_publications_by(publication_sequence);
 	var sorted_publication_html = generate_html(sorted_publications);
 	$("#publications").html(sorted_publication_html);
-	$("#current-sort-type").html(sort_Key);
 };
 
 $(document).ready(function() {
@@ -299,7 +63,3 @@ $(document).ready(function() {
 	setPublications(publication_info, sortKey);
 });
 
-$(".dropdown-item").click(function() {
-	var sortKey = this.innerText;
-	setPublications(publication_info, sortKey);
-});
