8ff6ea9207ae767bf81cbe8f2f0cce00ce04f226
bspitzbart <bradley.spitzbart@stonybrook.edu>
2018-11-15 15:51:51 -0500

    Update publications.js

--- Test/js/publications.js
+++ Test/js/publications.js
@@ -2,7 +2,7 @@ var publication_info = [
 	{
 		"title"		: "Harnessing high performance computing for large-scale imagery-enabled polar research",
 		"url"		: "EarthCube2018poster_imagery_redacted.pptx",
-		"authors"	: ["Heather J. Lynch" , "Shantenu Jha", "Bento Goncalves", "Bradley Spitzbart" , "Michael Willis", "Michael MacFerrin", " Vena Chu", "Mark Salvatore", "and Matteo Turilli".
+		"authors"	: ["Heather J. Lynch" , "Shantenu Jha", "Bento Goncalves", "Bradley Spitzbart" , "Michael Willis", "Michael MacFerrin", " Vena Chu", "Mark Salvatore", "and Matteo Turilli"],
 		"source"	: "EarthCube All-Hands Meeting, Washington, D.C. June 6-8, 2018",
 		"timestamp"	: "06/06/2018"
 	}
